78f9f36a9f9ecdd1bc20c3f5fbcd389aff32bfbf
Ignacio Ruben Ortuño Albarran <ignacioruben7@gmail.com>
2016-04-26 11:15:32 -0500

    Agregando un ancho a la caja de texto del formulario de articulos y validaciones

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,7 @@ class Article < ActiveRecord::Base
 	has_many :comments
 	has_attached_file :cover, styles: {thumb: "400x300", medium: "800x600" }
   	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+  	validates :title, presence: true, uniqueness: true, length: {maximum: 50}
+  	validates :body, presence: true, length: {minimum: 100}
+
 end
